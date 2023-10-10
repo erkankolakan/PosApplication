dfaf943f9d676ed7e7d70a913834af9d6df1ad16
Erkan Kolakan <erkankolakan@gmail.com>
2023-10-10 17:01:18 +0300

    uygulamanın temelleri atıldı

---
+++ client/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+//components
+import Header from './components/header/Header';
+
+
+function App() {
+  return (
+    <div >
+      <Header/>
+    </div>
+  );
+}
+
+export default App;
