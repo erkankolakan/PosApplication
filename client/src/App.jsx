c00013efd9404d32d1958e00ede5f65d54612f7f
Erkan Kolakan <erkankolakan@gmail.com>
2023-10-11 21:00:00 +0300

    kart sayfası yapıldı

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,34 +1,22 @@
-import React from 'react';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+
+// pages
+import Home from './pages/HomePage';
+import CartPage from './pages/CartPage';
 
 
-//components
-import Header from './components/header/Header';
-import Categories from './components/categories/Categories';
-import Products from './components/products/Products';
-import CartsTotals from './components/carts/CartTotals';
+
 
 
 function App() {
   return (
-    <>
-      <Header/>
-      <div className="home px-6 flex flex-col md:flex-row justify-between gap-10  md:pb-0 pb-16 ">
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/cart' element={<CartPage/>} />
+      </Routes>
+    </BrowserRouter>
 
-        <div className="categories  max-h-[85vh] snap-proximity overflow-y-auto">
-          <Categories/>
-        </div>
-
-        <div className="products flex-[8] h-[100vh] overflow-y-auto pb-0 md:pb-32">
-          <Products/>
-        </div>
-
-        <div className="cart-wraper min-w-[300px] md:-mr-[24px] md:-mt-[24px] border-l">
-          <div><CartsTotals/></div>
-        </div>
-
-      </div>
-
-    </>
   );
 }
 
---
+++ client/src/pages/HomePage.jsx
@@ -0,0 +1,30 @@
+//components
+import Header from "../components/header/Header";
+import Categories from "../components/categories/Categories";
+import Products from "../components/products/Products";
+import CartsTotals from "../components/carts/CartTotals";
+
+const HomePage = () => {
+  return (
+    <>
+      <Header />
+      <div className="home px-6 flex flex-col md:flex-row justify-between gap-10  md:pb-0 pb-16 ">
+        <div className="categories  max-h-[85vh] snap-proximity overflow-y-auto">
+          <Categories />
+        </div>
+
+        <div className="products flex-[8] h-[100vh] overflow-y-auto pb-0 md:pb-32">
+          <Products />
+        </div>
+
+        <div className="cart-wraper min-w-[300px] md:-mr-[24px] md:-mt-[24px] border-l">
+          <div>
+            <CartsTotals />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default HomePage;
