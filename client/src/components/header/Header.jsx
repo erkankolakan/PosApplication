ff216bfd87da065a9db5afd4ee9deac6999196e5
Erkan Kolakan <erkankolakan@gmail.com>
2023-10-12 21:39:32 +0300

    linklendirme sayfası yapıldı

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,6 +1,8 @@
 // icon
 import { SearchOutlined,HomeOutlined,ShoppingCartOutlined,CopyOutlined,UserOutlined,BarChartOutlined,LogoutOutlined } from '@ant-design/icons';
 
+import {Link} from "react-router-dom"
+
 import { Badge, Input } from 'antd';
 
 const Header = () => {
@@ -8,7 +10,7 @@ const Header = () => {
 <div className='border-b mb-6'>
         <header className='py-4 px-6 flex justify-between items-center gap-10'>
             <div className="logo">
-                <a href="/"><h2 className='text-2xl font-semibold md:text-4xl'>LOGO</h2></a>
+                <Link to="/"><h2 className='text-2xl font-semibold md:text-4xl'>LOGO</h2></Link>
 {/* tailwindcss de md: yazarkan 768px ve üstünde olacakları söylemiş oluyoruz. */}
             </div>
 
@@ -17,40 +19,40 @@ const Header = () => {
             </div>
 
             <div className="menu-links flex justify-between items-center gap-7 md:static fixed z-50 bottom-0 md:w-auto w-screen md:bg-transparent bg-white left-0 md:border-t-0 border-t md:px-0 px-4 py-3 ">
-                <a href={"/"} className='menu-link flex flex-col justify-center  hover:text-[#40a9ff] transition-all  items-center'>
+                <Link to={"/"} className='menu-link flex flex-col justify-center  hover:text-[#40a9ff] transition-all  items-center'>
                     <HomeOutlined className='md:text-2xl text-xl'/>
                     <span className='md:text-xs  text-[10px]'>Anasayfa</span>
-                </a>
+                </Link>
                 <Badge count={5} offset={[0,1]} className='md:flex hidden'>
-                    <a href={"/"} className='menu-link flex flex-col justify-center hover:text-[#40a9ff] transition-all   items-center'>
+                    <Link to={"/cart"} className='menu-link flex flex-col justify-center hover:text-[#40a9ff] transition-all   items-center'>
                             <ShoppingCartOutlined className='md:text-2xl text-xl'/> 
                             <span className='md:text-xs  text-[10px]'>Sepet</span>
-                    </a>
+                    </Link>
                 </Badge>
-                <a href={"/"} className='menu-link flex flex-col justify-center hover:text-[#40a9ff] transition-all   items-center'>
+                <Link to={"/"} className='menu-link flex flex-col justify-center hover:text-[#40a9ff] transition-all   items-center'>
                     <CopyOutlined className='md:text-2xl text-xl'/>
                     <span className='md:text-xs  text-[10px]'>Fatura</span>
-                </a>
-                <a href={"/"} className='menu-link flex flex-col justify-center hover:text-[#40a9ff] transition-all   items-center'>
+                </Link>
+                <Link to={"/"} className='menu-link flex flex-col justify-center hover:text-[#40a9ff] transition-all   items-center'>
                     <UserOutlined className='md:text-2xl text-xl'/>
                     <span className='md:text-xs  text-[10px]'>Müşteriler</span>
-                </a>
-                <a href={"/"} className='menu-link flex flex-col justify-center hover:text-[#40a9ff] transition-all   items-center'>
+                </Link>
+                <Link to={"/"} className='menu-link flex flex-col justify-center hover:text-[#40a9ff] transition-all   items-center'>
                     <BarChartOutlined  className='md:text-2xl text-xl'/>
                     <span className='md:text-xs  text-[10px]'>İstatistikler</span>
-                </a>
-                <a href={"/"} className='menu-link flex flex-col justify-center hover:text-[#40a9ff] transition-all   items-center'>
+                </Link>
+                <Link to={"/"} className='menu-link flex flex-col justify-center hover:text-[#40a9ff] transition-all   items-center'>
                     <LogoutOutlined className='md:text-2xl text-xl'/>
                     <span className='md:text-xs  text-[10px]'>Çıkış</span>
-                </a>
+                </Link>
             </div>
 
 
             <Badge count={5} offset={[0,1]} className='md:hidden flex'>
-                    <a href={"/"} className='menu-link flex flex-col justify-center hover:text-[#40a9ff] transition-all   items-center'>
+                    <Link to={"/"} className='menu-link flex flex-col justify-center hover:text-[#40a9ff] transition-all   items-center'>
                             <ShoppingCartOutlined className='text-2xl '/> 
                             <span className='md:text-xs  text-[10px]'>Sepet</span>
-                    </a>
+                    </Link>
                 </Badge>
 
 
