110657e2d25d65878d5a05c7b27f459ea83831cf
Erkan Kolakan <erkankolakan@gmail.com>
2023-10-17 18:19:21 +0300

    kategori CRUD işlemleri yapıldı

--- api/routes/categories.js
+++ api/routes/categories.js
@@ -4,6 +4,17 @@ const router = express.Router()
 
 
 
+router.get("/get-all", async (req , res) => {
+    try {
+        const categories = await Category.find()
+        res.send(categories)
+        // res.status(200).json(categories)
+    } catch (error) {
+        console.log(error);
+    }
+})
+
+
 router.post("/add-category" , async (req, res) => {
     try {
         const newCategory = new Category(req.body)
@@ -16,7 +27,15 @@ router.post("/add-category" , async (req, res) => {
     }
 } )
 
-
+router.delete("/delete-category", async (req , res) => {
+    try {
+        await Category.findOneAndDelete({ _id: req.body.categoryId})
+        res.status(200).json("Item deleted successfully");
+    } catch (error) {
+        // res.status(500).json({ error: "Error updating category" });
+        console.log(error);
+    }
+})
 
 
 module.exports = router
