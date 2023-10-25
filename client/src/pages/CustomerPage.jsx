1ea29252ad918e7c5b5ee5a855518f766dc17d03
Erkan Kolakan <erkankolakan@gmail.com>
2023-10-25 19:11:06 +0300

    müşteri sayfası

--- client/src/pages/CustomerPage.jsx
+++ client/src/pages/CustomerPage.jsx
@@ -1,38 +1,45 @@
+import { useEffect, useState } from "react";
 import Header from "../components/header/Header";
 import {Table } from "antd";
 
 
 const CustomerPage = () => {
-  const dataSource = [
-    {
-      key: "1",
-      name: "Mike",
-      age: 32,
-      address: "10 Downing Street",
-    },
-    {
-      key: "2",
-      name: "John",
-      age: 42,
-      address: "10 Downing Street",
-    },
-  ];
+
+  const [billItems, setBillItems] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const res = await fetch("http://localhost:5000/api/bill/get-all");
+        const data = await res.json();
+        await setBillItems(data);
+      } catch (error) {
+        console.log(error);
+      }
+    })();
+  }, []);
+
+
 
   const columns = [
     {
-      title: "Name",
-      dataIndex: "name",
-      key: "name",
+      title: "Müşteri Adı",
+      dataIndex: "customerName",
+      key: "customerName",
     },
     {
-      title: "Age",
-      dataIndex: "age",
-      key: "age",
+      title: "Telefon Numarası",
+      dataIndex: "costomerPhoneNumber",
+      key: "costomerPhoneNumber",
     },
     {
-      title: "Address",
-      dataIndex: "address",
-      key: "address",
+      title: "İşlem Tarihi",
+      dataIndex: "createdAt",
+      key: "createdAt",
+      render: (text) => {
+          return <span>{new Date(text).toLocaleDateString()}</span>;
+        },
+
     },
   ];
 
@@ -44,7 +51,12 @@ const CustomerPage = () => {
       <div className="px-6">
         <h1 className="text-4xl text-center font-semibold  mb-4">Müşterilerim</h1>
 
-        <Table dataSource={dataSource} columns={columns} bordered pagination={false} />
+        <Table 
+        scroll={{
+          x:1000,
+          y:300
+        }}
+        dataSource={billItems} columns={columns} bordered pagination={false} />
 
       </div>
 
